b07662a2f568efda8d220aaaaaa42538e7e0dad0
Christian Milbank <43425121+cmilbank@users.noreply.github.com>
2019-11-21 21:06:51 -0800

    Add files via upload

--- global.R
+++ global.R
@@ -1,6 +1,6 @@
 #set working directory
 
-setwd("C:/Users/cmilbank/Documents")
+#setwd("C:/Users/cmilbank/Documents")
 
 #Sys.setenv(JAVA_HOME = "H:/Decision Support/Stats Jam/R files/Java/jre1.8.0_201")
 
